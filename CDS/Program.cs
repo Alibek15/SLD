72c684d7293bf362d9ebdacaf8732e2114347204
Alibek <52044412+Alibek15@users.noreply.github.com>
2020-01-23 20:36:16 +0600

    Add files via upload

---
+++ CDS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace CDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage[] devices = new Storage[3];
+            devices[1] = new Flash();
+            devices[2] = new Flash();
+            devices[3] = new Flash();
+
+            ClassMenu menu = new ClassMenu(devices);
+            menu.Handler();
+        }
+    }
+}
---
+++ Library/File.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+     public class File 
+    {
+        private double _size;
+
+        public File()
+        {
+            Size = 780;
+        }
+
+        public File(double size)
+        {
+            Size = size;
+        }
+
+        public double Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+    }
+}
